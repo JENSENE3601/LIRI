650ffce6d0053053c8c7c27228c144263d04387e
JENSENE3601 <ericjensen3601@gmail.com>
2017-12-05 23:19:53 -0600

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: 'p03G1MHqXo4pNNLZatRRMWoGF',
+	consumer_secret: 'RS2MPQEgfR9igDVjT4X0ETDJgBS3vMG8VzkIeSE4bXGWJudxrQ',
+	access_token_key: '938168061311504384-h7THxiI4VgYUMFOia8a15h1qfkNQ0x1',
+	access_token_secret: 'JvAlx3LO1PJL2SaQoQoquoru2NDfCyK1oHuwyZckI4FHc',
+}
+
+exports.spotifyKeys = {
+	client_id: '8409209a5ff148b38d43b8753e985004',
+	client_secret: '20019cf1b21041949a921746d4962309',
+}
